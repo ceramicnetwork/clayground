3ba35635ce2356735eebd0d02c438bbb5e48f29a
Janko Simonovic <simonovic86@gmail.com>
2020-11-10 14:23:33 +0100

    fix: update destroy script

--- bin/docker-destroy-all.sh
+++ bin/docker-destroy-all.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # Stop all containers
-containers=$(docker ps -a -q)
+containers=$(docker ps -a -q --filter="name=clayground*")
 if [ -n "$containers" ] ; then
         docker stop $containers
 fi
 # Delete all containers
-containers=$(docker ps -a -q)
+containers=$(docker ps -a -q --filter="name=clayground*" )
 if [ -n "$containers" ]; then
         docker rm -f -v $containers
 fi
 # Delete all images
-images=$(docker images -q -a)
+images=$(docker images "clayground*")
 if [ -n "$images" ]; then
         docker rmi -f $images
 fi
