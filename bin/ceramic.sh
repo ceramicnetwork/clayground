a5cbce975883dd00c8428d1c86277030bf2441c5
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-02-04 21:13:14 -0500

    clean: remove unneeded scripts

--- bin/ceramic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CLAYGROUND_BIN_PATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 || exit ; pwd -P )"
-CLAYGROUND_ROOT_PATH="$(dirname "$CLAYGROUND_BIN_PATH")"
-
-node "$CLAYGROUND_ROOT_PATH/components/ceramic/packages/cli/lib/bin/ceramic.js" "$@"
